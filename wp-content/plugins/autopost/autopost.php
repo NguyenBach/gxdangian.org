8de6fae7d811cd0abcb9aeb9d1748685da42e794
NguyenBach <bachnq214@gmail.com>
2017-07-11 10:41:10 +0700

    update plugin

--- wp-content/plugins/autopost/autopost.php
+++ wp-content/plugins/autopost/autopost.php
@@ -20,7 +20,6 @@ require_once 'simple_html_dom.php';
 function autopost_add_post()
 {
     $html = file_get_html('http://dongten.net/noidung/category/loi-chua-cho-ngay-song');
-    $content = $html->find('#content');
     $content = $html->getElementById('content');
     $id = [];
     foreach ($content->childNodes() as $div) {
@@ -28,9 +27,9 @@ function autopost_add_post()
         $divid = $div->id;
         $id[] = explode('-', $divid)[1];
     }
-    for($i = 0; $i < $id ; $i++){
-        $html = file_get_html('http://dongten.net/noidung/' . $id[$i]);
-        $postContent = $html->getElementById('post-' . $id[$i]);
+//    for($i = 0; $i < $id ; $i++){
+        $html = file_get_html('http://dongten.net/noidung/' . $id[0]);
+        $postContent = $html->getElementById('post-' . $id[0]);
         $title = $postContent->find('.entry-title');
         $contentPost = $postContent->childNodes(2);
         $contentPost->childNodes(1)->find('a')[0]->outertext = '';
@@ -40,6 +39,21 @@ function autopost_add_post()
             'post_content' => $contentPost,
         ];
         wp_insert_post($post,true);
-    }
+//    }
 }
-register_activation_hook(__FILE__,'autopost_add_post');
+register_activation_hook(__FILE__, 'my_activation');
+add_action('my_daily_event', 'do_this_daily');
+
+function my_activation() {
+//    autopost_add_post();
+    wp_schedule_event( current_time( 'timestamp' ), 'daily', 'my_daily_event');
+}
+
+function do_this_daily() {
+   autopost_add_post();
+}
+register_deactivation_hook(__FILE__, 'my_deactivation');
+
+function my_deactivation() {
+    wp_clear_scheduled_hook('my_daily_event');
+}
