a7fb7d63afff0018180ab019327964b16dc8f1df
NguyenBach <bachnq214@gmail.com>
2017-07-12 09:35:46 +0700

    update plugin

--- wp-content/plugins/autopost/autopost.php
+++ wp-content/plugins/autopost/autopost.php
@@ -17,9 +17,11 @@ Text Domain: akismet
 */
 require_once 'simple_html_dom.php';
 
-function autopost_add_post()
+global $lastpostid;
+function autopost_add_post($linkCategory,$category,$slug)
 {
-    $html = file_get_html('http://dongten.net/noidung/category/loi-chua-cho-ngay-song');
+    global $lastpostid;
+    $html = file_get_html($linkCategory);
     $content = $html->getElementById('content');
     $id = [];
     foreach ($content->childNodes() as $div) {
@@ -27,34 +29,74 @@ function autopost_add_post()
         $divid = $div->id;
         $id[] = explode('-', $divid)[1];
     }
-//    for($i = 0; $i < $id ; $i++){
-        $html = file_get_html('http://dongten.net/noidung/' . $id[0]);
-        $postContent = $html->getElementById('post-' . $id[0]);
-        $title = $postContent->find('.entry-title');
-        $contentPost = $postContent->childNodes(2);
-        $contentPost->childNodes(1)->find('a')[0]->outertext = '';
-        $post = [
-            'post_title' => $title,
-            'post_status'   => 'publish',
-            'post_content' => $contentPost,
-            'post_category' => array( get_cat_ID( "Lời Chúa Mỗi Ngày" ) )
-        ];
-        wp_insert_post($post,true);
-//    }
+    if(isset($lastpostid[$slug]) && $lastpostid[$slug] == $id[0]){
+        return;
+    }
+    $lastpostid[$slug] = $id[0];
+    $html = file_get_html('http://dongten.net/noidung/' . $id[0]);
+    $postContent = $html->getElementById('post-' . $id[0]);
+    $title = $postContent->find('.entry-title')[0];
+    $contentPost = $postContent->childNodes(2);
+    $a = $contentPost->childNodes(1)->find('a')[0];
+    $l = $a->find('img')[0]->src;
+    $l = explode('?',$l)[0];
+//    $contentPost->childNodes(1)->find('a')[0]->outertext = '';
+    $post = [
+        'post_title' => $title,
+        'post_status' => 'publish',
+        'post_content' => $contentPost,
+        'post_category' => array(get_cat_ID($category))
+    ];
+    $post_id = wp_insert_post($post, true);
+    Generate_Featured_Image($l, $post_id);
 }
+
 register_activation_hook(__FILE__, 'my_activation');
 add_action('my_daily_event', 'do_this_daily');
 
-function my_activation() {
+function my_activation()
+{
 //    autopost_add_post();
-    wp_schedule_event( current_time( 'timestamp' ), 'daily', 'my_daily_event');
+    wp_schedule_event(current_time('timestamp'), 'daily', 'my_daily_event');
 }
 
-function do_this_daily() {
-   autopost_add_post();
+function do_this_daily()
+{
+    autopost_add_post('http://dongten.net/noidung/category/loi-chua-cho-ngay-song',"Lời Chúa Mỗi Ngày",'loi-chua-moi-ngay');
+    autopost_add_post('http://dongten.net/noidung/category/hoc-lam-nguoi',"Học Làm Người",'hoc-lam-nguoi');
+    autopost_add_post('http://dongten.net/noidung/category/hoc-lam-nguoi/le-song',"Lẽ Sống",'le-song');
+    autopost_add_post('http://dongten.net/noidung/category/hoc-lam-nguoi',"Học Làm Người",'hoc-lam-nguoi');
+    autopost_add_post('http://dongten.net/noidung/category/phuc-vu-duc-tin/duc-tin-va-nguoi-tre',"Đức Tin Và Người Trẻ",'duc-tin');
+
 }
+
 register_deactivation_hook(__FILE__, 'my_deactivation');
 
-function my_deactivation() {
+function my_deactivation()
+{
     wp_clear_scheduled_hook('my_daily_event');
+}
+
+function Generate_Featured_Image($image_url, $post_id)
+{
+    $upload_dir = wp_upload_dir();
+    $image_data = file_get_contents($image_url);
+    $filename = basename($image_url);
+    if (wp_mkdir_p($upload_dir['path'])) $file = $upload_dir['path'] . '/' . $filename;
+    else                                    $file = $upload_dir['basedir'] . '/' . $filename;
+    file_put_contents($file, $image_data);
+    $wp_filetype = wp_check_filetype($filename, null);
+    $attachment = array(
+        'post_mime_type' => $wp_filetype['type'],
+        'post_title' => sanitize_file_name($filename),
+        'post_content' => '',
+        'post_status' => 'inherit'
+    );
+    $attach_id = wp_insert_attachment($attachment, $file);
+    require_once(ABSPATH . 'wp-admin/includes/image.php');
+    $attach_data = wp_generate_attachment_metadata($attach_id, $file);
+    wp_update_attachment_metadata($attach_id, $attach_data);
+    update_post_meta( $post_id, '_thumbnail_id', $attach_id );
+    set_post_thumbnail( $post_id, $attach_id );
+
 }
