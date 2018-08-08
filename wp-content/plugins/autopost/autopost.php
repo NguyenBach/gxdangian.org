addc44c3d6197a58350c74e7098aaa4a3f13273c
BachNQ <bachnq214@gmail.com>
2018-08-08 20:48:10 +0700

    autopost

--- wp-content/plugins/autopost/autopost.php
+++ wp-content/plugins/autopost/autopost.php
@@ -16,42 +16,9 @@ License: GPLv2 or later
 Text Domain: akismet
 */
 require_once 'simple_html_dom.php';
+require_once "tool.php";
 
-global $lastpostid;
-function autopost_add_post($linkCategory,$category,$slug)
-{
-    global $lastpostid;
-    $html = file_get_html($linkCategory);
-    $content = $html->getElementById('content');
-    $id = [];
-    foreach ($content->childNodes() as $div) {
-        if ($div->class == 'pagenavi clear' || $div->class == "heading") continue;
-        $divid = $div->id;
-        $id[] = explode('-', $divid)[1];
-    }
-    if(isset($lastpostid[$slug]) && $lastpostid[$slug] == $id[0]){
-        return;
-    }
-    $lastpostid[$slug] = $id[0];
-    $html = file_get_html('http://dongten.net/noidung/' . $id[0]);
-    $postContent = $html->getElementById('post-' . $id[0]);
-    $title = $postContent->find('.entry-title')[0];
-    $contentPost = $postContent->childNodes(2);
-    $fuck = 0;
-    if($slug == 'loichua') $fuck = 1;
-    $a = $contentPost->childNodes($fuck)->find('a')[0];
-    $l = $a->find('img')[0]->src;
-    $l = explode('?',$l)[0];
-    $contentPost->childNodes($fuck)->find('a')[0]->outertext = '';
-    $post = [
-        'post_title' => $title,
-        'post_status' => 'publish',
-        'post_content' => $contentPost,
-        'post_category' => array(get_cat_ID($category))
-    ];
-    $post_id = wp_insert_post($post);
-    Generate_Featured_Image($l, $post_id);
-}
+
 
 register_activation_hook(__FILE__, 'my_activation');
 add_action('my_daily_event', 'do_this_daily');
@@ -64,10 +31,10 @@ function my_activation()
 
 function do_this_daily()
 {
-    autopost_add_post('http://dongten.net/noidung/category/loi-chua-cho-ngay-song',"Lời Chúa Mỗi Ngày",'loichua');
-    autopost_add_post('http://dongten.net/noidung/category/hoc-lam-nguoi',"Học Làm Người",'hoclamnguoi');
-    autopost_add_post('http://dongten.net/noidung/category/hoc-lam-nguoi/le-song',"Lẽ Sống",'lesong');
-    autopost_add_post('http://dongten.net/noidung/category/phuc-vu-duc-tin/duc-tin-va-nguoi-tre',"Đức Tin Và Người Trẻ",'ductin');
+    autopost_add_post('http://dongten.net/noidung/category/loi-chua-cho-ngay-song',"Lời Chúa Mỗi Ngày");
+    autopost_add_post('http://dongten.net/noidung/category/hoc-lam-nguoi',"Học Làm Người");
+    autopost_add_post('http://dongten.net/noidung/category/hoc-lam-nguoi/le-song',"Lẽ Sống");
+    autopost_add_post('http://dongten.net/noidung/category/phuc-vu-duc-tin/duc-tin-va-nguoi-tre',"Đức Tin Và Người Trẻ");
 
 }
 
@@ -80,26 +47,3 @@ function my_deactivation()
     $lastpostid = [];
 }
 
-function Generate_Featured_Image($image_url, $post_id)
-{
-    $upload_dir = wp_upload_dir();
-    $image_data = file_get_contents($image_url);
-    $filename = basename($image_url);
-    if (wp_mkdir_p($upload_dir['path'])) $file = $upload_dir['path'] . '/' . $filename;
-    else                                    $file = $upload_dir['basedir'] . '/' . $filename;
-    file_put_contents($file, $image_data);
-    $wp_filetype = wp_check_filetype($filename, null);
-    $attachment = array(
-        'post_mime_type' => $wp_filetype['type'],
-        'post_title' => sanitize_file_name($filename),
-        'post_content' => '',
-        'post_status' => 'inherit'
-    );
-    $attach_id = wp_insert_attachment($attachment, $file);
-    require_once(ABSPATH . 'wp-admin/includes/image.php');
-    $attach_data = wp_generate_attachment_metadata($attach_id, $file);
-    wp_update_attachment_metadata($attach_id, $attach_data);
-    update_post_meta( $post_id, '_thumbnail_id', $attach_id );
-    set_post_thumbnail( $post_id, $attach_id );
-
-}
--- wp-content/plugins/autopost/tool.php
+++ wp-content/plugins/autopost/tool.php
@@ -7,18 +7,115 @@
  */
 require_once 'simple_html_dom.php';
 
-$html = file_get_html('http://dongten.net/noidung/category/loi-chua-cho-ngay-song');
-$content = $html->find('#content');
-$content = $html->getElementById('content');
-$id = [];
-foreach ($content->childNodes() as $div){
-    if($div->class == 'pagenavi clear' || $div->class == "heading" ) continue;
-    $divid = $div->id;
-    $id[] = explode('-',$divid)[1];
-}
-$html = file_get_html('http://dongten.net/noidung/'.$id[0]);
-$postContent = $html->getElementById('post-'.$id[0]);
-$title = $postContent->find('.entry-title');
-$contentPost = $postContent->childNodes(2);
-$contentPost->childNodes(1)->find('a')[0]->outertext = '';
 
+$base = 'https://dongten.net/category/cau-nguyen/loi-chua-cho-ngay-song/';
+function getHtml($url)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
+}
+
+function getPostUrl($output)
+{
+    $html = str_get_html($output);
+    $main_content = $html->find('#main-content')[0];
+    $item_lists = $main_content->find('.item-list');
+    $item = $item_lists[0];
+    $title = $item->find('.post-box-title')[0];
+    $a = $title->find('a')[0];
+    $url = $a->href;
+    return $url;
+}
+
+function getImgUrl($output)
+{
+    $html = str_get_html($output);
+    $main_content = $html->find('#main-content')[0];
+    $item_lists = $main_content->find('.item-list');
+    $item = $item_lists[0];
+    $thumnail = $item->find('.post-thumbnail')[0];
+    $img = $thumnail->find('img')[0];
+    $img_url = $img->src;
+    return $img_url;
+}
+
+function getPost($url)
+{
+    $output = getHtml($url);
+    $html = str_get_html($output);
+    $post = $html->find('#the-post')[0];
+    $title = $post->find('h1.name')[0];
+    $title = $title->plaintext;
+    $content = $post->find('div.entry')[0];
+    $content = $content->innertext;
+    $post = [
+        'post_title' => $title,
+        'post_content' => $content,
+    ];
+    return $post;
+
+}
+
+function Generate_Featured_Image($image_url, $post_id)
+{
+    $upload_dir = wp_upload_dir();
+    $image_data = file_get_contents($image_url);
+    $filename = basename($image_url);
+    if (wp_mkdir_p($upload_dir['path'])) $file = $upload_dir['path'] . '/' . $filename;
+    else                                    $file = $upload_dir['basedir'] . '/' . $filename;
+    file_put_contents($file, $image_data);
+    $wp_filetype = wp_check_filetype($filename, null);
+    $attachment = array(
+        'post_mime_type' => $wp_filetype['type'],
+        'post_title' => sanitize_file_name($filename),
+        'post_content' => '',
+        'post_status' => 'inherit'
+    );
+    $attach_id = wp_insert_attachment($attachment, $file);
+    require_once(ABSPATH . 'wp-admin/includes/image.php');
+    $attach_data = wp_generate_attachment_metadata($attach_id, $file);
+    wp_update_attachment_metadata($attach_id, $attach_data);
+    update_post_meta($post_id, '_thumbnail_id', $attach_id);
+    set_post_thumbnail($post_id, $attach_id);
+
+}
+
+function checkPostExist($title, $content = '')
+{
+    if ($content != '') {
+        $check = post_exists($title, $content);
+    } else {
+        $check = post_exists($title);
+    }
+    return $check;
+
+}
+
+function mailToMe($title,$url,$error){
+    $to = 'bachnq214@gmail.com';
+    $subject = " Get Post Error!";
+    $message = "Get post" .$title . ' at '. $url. '. Error: ' .$error;
+    wp_mail($to,$subject,$message);
+}
+
+function autopost_add_post($linkCategory,$category)
+{
+    $output = getHtml($linkCategory);
+    $postUrl = getPostUrl($output);
+    $postThumbnailImgUrl = getImgUrl($output);
+    $postContent = getHtml($postUrl);
+    $post = getPost($postContent);
+    $post['post_status'] = 'publish';
+    $post['post_category'] = array(get_cat_ID($category));
+    $check = checkPostExist($post['post_title']);
+    if(!$check){
+        mailToMe($post['post_title'],$postUrl,'Post Exist');
+        return;
+    }
+    $post_id = wp_insert_post($post);
+    Generate_Featured_Image($postThumbnailImgUrl, $post_id);
+}
